6ba3743cfd121d17cccc83df2152244bf66d75ec
zakabdu205 <zakabdu205@hotmail.com>
2022-08-14 14:49:16 +0800

    test

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "e13df200ca80a4f7"; }
+/******/ 		__webpack_require__.h = function() { return "6e5ad85308a61d2d"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- components/blog_layout.js
+++ components/blog_layout.js
@@ -15,15 +15,15 @@ export default function BlogLayout() {
         <h1 className="font-bold mb-5 text-2xl">News and Articles</h1>
 
         <div className="card mb-auto w-96 bg-base-100 shadow-xl">
-          <Link href={"/"}>
-            <figure>
-              <img src="https://placeimg.com/400/225/arch" alt="Shoes" />
-            </figure>
-            <div className="card-body">
-              <h2 className="card-title mx-auto">Text-test</h2>
-              <p>description of the post should be here</p>
-            </div>
-          </Link>
+          <figure>
+            <img src="https://placeimg.com/400/225/arch" alt="Shoes" />
+          </figure>
+          <div className="card-body">
+            <h2 className="card-title mx-auto">
+              <Link href={"/"}>Text-test</Link>
+            </h2>
+            <p>description of the post should be here</p>
+          </div>
         </div>
       </div>
     </div>
